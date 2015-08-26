cafdca7d01e716a2475fae656cdcfcffcc83bf45
Nate Morse <morse.nate@gmail.com>
2015-08-25 20:54:20 -0400

    turtles all the way down

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
+function Turtlearium() {
+}
+Turtlearium.annotations = [
+  new angular.ComponentAnnotation({
+    selector: 'turtlearium',
+  }),
+  new angular.ViewAnnotation({
+    template: '<canvas></canvas>'
+  })
+];
+
+function Honeybee() {
+}
+Honeybee.annotations = [
+  new angular.ComponentAnnotation({
+    selector: 'honeybee',
+  }),
+  new angular.ViewAnnotation({
+    template: '<graphlet>graphlet</graphlet>'
+  })
+];
+
 function App() {
 }
-
 App.annotations = [
-  new angular.Component({
+  new angular.ComponentAnnotation({
     selector: 'app',
   }),
-  new angular.View({
-    template: 'Hello world!'
+  new angular.ViewAnnotation({
+    directives: [Honeybee, Turtlearium],
+    template: '<menu>menu</menu><honeybee></honeybee><turtlearium></turtlearium>'
   })
 ];

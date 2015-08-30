f8487dd67649e54ec41eb12139e5f485194f5ec6
Nate Morse <morse.nate@gmail.com>
2015-08-29 22:36:46 -0400

    a few more components

--- ent-app.js
+++ ent-app.js
@@ -10,6 +10,38 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var angular2_1 = require('angular2/angular2');
+var HoneyBee = (function () {
+    function HoneyBee(gs) {
+        this.graph = gs.graphel;
+    }
+    HoneyBee = __decorate([
+        angular2_1.Component({
+            appInjector: [GraphService],
+            selector: 'honeybee'
+        }),
+        angular2_1.View({
+            template: '<h2>Honey Bee</h2>\
+  {{graph.nodes[0].id}}\
+  '
+        }), 
+        __metadata('design:paramtypes', [])
+    ], HoneyBee);
+    return HoneyBee;
+})();
+var BeeHive = (function () {
+    function BeeHive() {
+    }
+    BeeHive = __decorate([
+        angular2_1.Component({
+            selector: 'beehive'
+        }),
+        angular2_1.View({
+            template: '<h2>Bee Hive</h2>'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], BeeHive);
+    return BeeHive;
+})();
 var AppComponent = (function () {
     function AppComponent() {
     }
@@ -18,10 +50,21 @@ var AppComponent = (function () {
             selector: 'ent-app'
         }),
         angular2_1.View({
-            template: '<h1>first Angular 2 App using TypeScript</h1>'
+            directives: [HoneyBee, BeeHive],
+            template: '\
+    <h1>first Angular 2 App using TypeScript</h1>\
+    <honeybee></honeybee>\
+    <beehive></beehive>\
+  '
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
+})();
+var GraphService = (function () {
+    function GraphService() {
+        this.graphel = { graph: {}, nodes: [{ id: 'a' }, { id: 'b' }], edges: [{ from: 'a', to: 'b' }] };
+    }
+    return GraphService;
 })();
 angular2_1.bootstrap(AppComponent);
--- ent-app.ts
+++ ent-app.ts
@@ -1,10 +1,50 @@
 import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  appInjector: [GraphService],
+  selector: 'honeybee'
+})
+@View({
+  template: '<h2>Honey Bee</h2>\
+  {{graph.nodes[0].id}}\
+  '
+})
+class HoneyBee {
+  constructor (gs: GraphService) {
+    this.graph = gs.graphel;
+  }
+}
+
+@Component({
+  selector: 'beehive'
+})
+@View({
+  template: '<h2>Bee Hive</h2>'
+})
+class BeeHive {
+}
+
+
 @Component({
   selector: 'ent-app'
 })
 @View({
-  template: '<h1>first Angular 2 App using TypeScript</h1>'
+  directives: [HoneyBee, BeeHive],
+  template: '\
+    <h1>first Angular 2 App using TypeScript</h1>\
+    <honeybee></honeybee>\
+    <beehive></beehive>\
+  '
 })
 class AppComponent {
 }
+
+
+class GraphService {
+  graphel: Array<string>;
+  constructor() {
+    this.graphel = {graph:{}, nodes:[{id:'a'}, {id:'b'}], edges:[{from:'a', to:'b'}]};
+  }
+}
+
 bootstrap(AppComponent);
